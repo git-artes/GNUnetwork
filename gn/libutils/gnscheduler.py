e70dd93174bf8519bc237704b589f7aea17d0e10
Víctor González Barbone <vagonbar@gmail.com>
2013-10-29 14:53:48 -0200

    libutils: scheduler, examples by type, by queue length

--- gn/libutils/gnscheduler.py
+++ gn/libutils/gnscheduler.py
@@ -6,7 +6,7 @@
 '''Classes and Functions to implement a generic scheduler.
 '''
 
-import threading  #, Queue
+import threading
 
 
 class Scheduler(threading.Thread):
@@ -15,11 +15,11 @@ class Scheduler(threading.Thread):
     This scheduler gets one element from one of several input queues, and puts elements in one or several output queues. Behaviour is regulated by a scheduling function which is expected to be overwritten when subclassing this class. Selection of input queue to get element from, processing, creation of one or more elements of same or different type, and putting elements in output queues are all regulated by this scheduling function.
     '''
  
-    def __init__(self, in_queues, dc_outqueues):
+    def __init__(self, in_queues, out_queues):
         '''Constructor.
         
-        @param in_queues: an input queue of a list of input queues from which items are extracted.
-        @param dc_outqueues: a dictionary of key nm_queue, the name of an output; value may be a queue, a tuple (function, queue) or other structure to be processed by the scheduling function fn_sched, which must be overwritten in a subclass.
+        @param in_queues: a list of input queues from which items are extracted. If input queues are given within a more elaborate structure, functin run() must be overwritten.
+        @param out_queues: a structure containing the output queues. A possible structure is a dictionary of key nm_queue, the name of an output queue; value may be a queue, a tuple (function, queue) or other structure to be processed by the scheduling function fn_sched, which must be overwritten.
         '''
         threading.Thread.__init__(self)
         self.daemon = True
@@ -28,10 +28,12 @@ class Scheduler(threading.Thread):
             self.in_queues = in_queues
         else:
             self.in_queues = [in_queues]
-        self.dc_outqueues = dc_outqueues    # output queues in a dictionary
+        self.out_queues = out_queues    # output queues in a dictionary
         return
 
     def fn_sched(self):
+        '''A dummy scheduling function; to be overwritten in a subclass.
+        '''
         pass
         return
 
@@ -42,7 +44,7 @@ class Scheduler(threading.Thread):
         while not self.finished:
             self.fn_sched()
         else:
-            print 'Scheduler, stopped'
+            #print 'Scheduler, stopped'
             self.stop()
         for in_qu in self.in_queues:
             in_qu.join()

8b67923426d15ac00ce9ff7360010a6eccc3772a
Víctor González Barbone <vagonbar@gmail.com>
2013-11-02 20:25:41 -0200

    libadaptationlayer: schedFrToEv, minor doc corrections

--- gn/libutils/__init__.py
+++ gn/libutils/__init__.py
@@ -1,16 +1,7 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*
 
-# GNU network __init__ package file
-
-'''A library for utils.
-
-Package contents:
-    - gnscheduler
-Developed at::
-    Departamento de Telecomunicaciones
-    Instituto de Ingeniería Eléctrica
-    Facultad de Ingeniería
-    Universidad de la República
-    Montevideo, Uruguay.
+'''Utility classes and functions for GNU network.
 '''
+
+

aacf8cc6394bd26337e773e1336427b30343f9c0
Víctor González Barbone <vagonbar@gmail.com>
2013-11-27 14:54:03 -0200

    libevents2: a new version of libevents

---
+++ gn/libevents2/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# GNU network __init__ package file
+
+'''A library for events management.
+
+Package contents:
+    - events: classes and functions for events.
+
+Developed at::
+    Departamento de Telecomunicaciones
+    Instituto de Ingeniería Eléctrica
+    Facultad de Ingeniería
+    Universidad de la República
+    Montevideo, Uruguay.
+'''
---
+++ gn/libevents2/if_events.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''An interface for the libevents library of Events.
+
+'''
+
+import events as events
+
+
+def mkevent(pnickname=None, pframe=None):
+    '''Creates an event from a nickname or from a frame.
+    
+    @param pnickname: the event nickname.
+    @param pframe: a frame in bin dta format (confirm!)
+    @return: an Event object.
+    '''
+    if not pnickname and not pframe:
+        raise EventNameException('No nickname or frame received.')
+        return None
+    if pnickname:
+        pass
+        return  # ...an Event object
+    if pframe:
+        nickname,src_addr,dst_addr = pframe.split(',')
+        ev= events.mkevent(nickname)
+        ev.src_addr=src_addr
+        ev.dst_addr = dst_addr
+        return  ev
+
+
+def mkframe(ev_obj):
+    '''Returns a frame with the event information.
+    
+    @param ev_obj: an Event object.
+    @return: a frame in bin data format (confirm!).
+    '''
+    if not isinstance(ev_obj, events.Event):
+        raise EventNameException('Parameter is not an Event object.')
+        return None
+    frame = ""+ev_obj.ev_nickname+","+ev_obj.src_addr+","+ev_obj.dst_addr
+    return frame
+
+
+def test():
+    ev = events.mkevent("MgmtBeacon")
+    ev.src_addr = "100"
+    ev.dst_addr=  "150"
+
+    frame=mkframe(ev)
+    print " Frame = :", frame
+
+    event=mkevent(pframe=frame) 
+    print "Event =", event     
+
+if __name__ == '__main__':
+    try:
+        test()
+    except KeyboardInterrupt:
+        pass
+        
+
+

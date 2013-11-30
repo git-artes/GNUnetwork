9dbd2d7bd69ec624eb68ca7a16f7cffd20510be1
Víctor González Barbone <vagonbar@gmail.com>
2013-11-30 12:54:14 -0200

    libevents: if_events changed to events v2

--- gn/libevents/if_events.py
+++ gn/libevents/if_events.py
@@ -1,36 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-'''An interface for the libevents library of Events.
+'''Interface for libevents library with string based test frames.
 
+This interface module defines two functions, mkevent() to make an Event object from a nickname and frame, and mkframe() to make a string frame from an Event object.
+
+Proposed name for this module: if_events_strfrm.
 '''
 
 import events as events
 
 
 def mkevent(pnickname=None, pframe=None):
-    '''Creates an event froma nickname or from a frame.
+    '''Creates an event from a nickname or from a frame.
     
     @param pnickname: the event nickname.
     @param pframe: a frame in bin dta format (confirm!)
     @return: an Event object.
     '''
     if not pnickname and not pframe:
-        raise EventNameException('No nickname or frame received.')
+        raise events.EventNameException('No nickname or frame received.')
         return None
     if pnickname:
         pass
         return  # ...an Event object
     if pframe:
-        try:
-            nickname,src_addr,dst_addr = pframe.split(',')
-            ev= events.mkevent(nickname)
-            ev.src_addr=src_addr
-            ev.dst_addr = dst_addr
-            return  ev
-        except:
-            return None
-            
+        ev_dc = {}
+        nickname, ev_dc['src_addr'], ev_dc['dst_addr'] = pframe.split(',')
+        ev = events.mkevent(nickname, frmpkt=pframe, ev_dc=ev_dc)
+        #ev.src_addr=src_addr
+        #ev.dst_addr = dst_addr
+        return  ev
 
 
 def mkframe(ev_obj):
@@ -42,20 +42,21 @@ def mkframe(ev_obj):
     if not isinstance(ev_obj, events.Event):
         raise EventNameException('Parameter is not an Event object.')
         return None
-    frame = ""+ev_obj.ev_nickname+","+ev_obj.src_addr+","+ev_obj.dst_addr
+    frame = "" + ev_obj.nickname + "," + \
+        ev_obj.ev_dc['src_addr'] + "," + ev_obj.ev_dc['dst_addr']
     return frame
 
 
 def test():
-    ev = events.mkevent("MgmtBeacon")
-    ev.src_addr = "100"
-    ev.dst_addr=  "150"
-
+    ev = events.mkevent("MgmtBeacon", ev_dc={})
+    ev.ev_dc['src_addr'] = "100"
+    ev.ev_dc['dst_addr'] =  "150"
+    print "=== Event to frame:"; print ev
     frame=mkframe(ev)
-    print " Frame = :", frame
-
-    event=mkevent(pframe=frame) 
-    print "Event =", event     
+    print "=== Frame:", frame
+    evfromframe = mkevent(pframe=frame) 
+    print "=== Event from frame:"; print evfromframe
+    return
 
 if __name__ == '__main__':
     try:

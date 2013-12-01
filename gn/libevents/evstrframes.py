019da0a27eeb3d6041758138374f54eb33be31fc
Víctor González Barbone <vagonbar@gmail.com>
2013-12-01 21:31:34 -0200

    libevents, add new module evstrframes

---
+++ gn/libevents/evstrframes.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''Use of events with string frames.
+
+This module defines two functions, mkevent() to make an Event object from a nickname and frame, and mkframe() to make a string frame from an Event object.
+'''
+
+import events
+import if_events
+
+
+def mkevent(pnickname=None, pframe=None):
+    '''Creates an event from a nickname or from a frame.
+    
+    @param pnickname: the event nickname.
+    @param pframe: a frame in bin dta format (confirm!)
+    @return: an Event object.
+    '''
+    if not pnickname and not pframe:
+        raise events.EventNameException('No nickname or frame received.')
+        return None
+    if pnickname:
+        pass
+        return  # ...an Event object
+    if pframe:
+        ev_dc = {}
+        nickname, ev_dc['src_addr'], ev_dc['dst_addr'] = pframe.split(',')
+        ev = if_events.mkevent(nickname, frmpkt=pframe, ev_dc=ev_dc)
+        #ev.src_addr=src_addr
+        #ev.dst_addr = dst_addr
+        return  ev
+
+
+def mkframe(ev_obj):
+    '''Returns a frame with the event information.
+    
+    @param ev_obj: an Event object.
+    @return: a frame in bin data format (confirm!).
+    '''
+    if not isinstance(ev_obj, events.Event):
+        raise EventNameException('Parameter is not an Event object.')
+        return None
+    frame = "" + ev_obj.nickname + "," + \
+        ev_obj.ev_dc['src_addr'] + "," + ev_obj.ev_dc['dst_addr']
+    return frame
+
+
+def test():
+    ev = events.mkevent("MgmtBeacon", ev_dc={})
+    ev.ev_dc['src_addr'] = "100"
+    ev.ev_dc['dst_addr'] =  "150"
+    print "=== Event to frame:"; print ev
+    frame=mkframe(ev)
+    print "=== Frame:", frame
+    evfromframe = mkevent(pframe=frame) 
+    print "=== Event from frame:"; print evfromframe
+    return
+
+if __name__ == '__main__':
+    try:
+        test()
+    except KeyboardInterrupt:
+        pass
+        
+
+

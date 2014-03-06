aaff8150d72683b22cf4f7ddba9c804c77dcbabf
Víctor González Barbone <vagonbar@gmail.com>
2014-03-05 22:09:32 -0200

    libadaptlay80211: schedEvToFr, schedFrToEv ready, passing tests

--- gn/libadaptlay80211/schedEvToFr.py
+++ gn/libadaptlay80211/schedEvToFr.py
@@ -5,23 +5,21 @@
 #
 
 
-'''An event to frame scheduler, from Layer 2 into Layer 1.
+'''An event to frame scheduler based on IEEE 802.11 frames.
 
 A scheduler that gets an event from a Layer 2 input queue, generates the corresponding frame, and puts it into the Layer 1 transmit queue.
 '''
+
 import Queue
 import sys
-
 sys.path +=['..']
 
-import libutils.gnscheduler as Scheduler
+import libutils.gnscheduler as gnscheduler
 import libevents.if_events as if_events
 
-# The next import is defined only for testing
-#import libevents.evstrframes as evstrframes
 
 
-class SchedEvToFr(Scheduler.Scheduler):
+class SchedEvToFr(gnscheduler.Scheduler):
     '''Subclass of Scheduler for adapting Layers 1 and 2.
     '''
 
@@ -29,66 +27,25 @@ class SchedEvToFr(Scheduler.Scheduler):
         '''Scheduling function to process an events queue.
         
         Scheduling function to process an events queue: reads one element from the input event queue, generates a frame, and puts this frame in  the output queue.
+        
         out_queues: a dictionary. 
         '''
         in_qu = self.in_queues[0]
         event = in_qu.get(True)
-        #print 'SchedEvToFr, event', event
 
-        ###
-        #frame = evstrframes.mkframe(event)
-        frame = event.mkframepkt()       
+        frmobj = if_events.evtofrm(event)
+        frame = frmobj.mkpkt()       
 
         out_queue = self.out_queues['frames']
         out_queue.put(frame, False)   # add to queue, don't block 
-        #print " out queue size ", out_queue.qsize()
         in_qu.task_done()
 
 
-
-def test():
-    '''Tests SchedEvToFr class.
-
-    Put some events in input queue to read, create frames and put in output queue.
-    '''
-    frame_q = Queue.Queue(10)
-    ev_q = Queue.Queue(10)
-    
-    out_queues = { \
-        'frames':    (frame_q) \
-    }
-    sch = SchedEvToFr(ev_q, out_queues)
-    
- 
-    # create events and put in input queue
-    for name in ['MgmtBeacon', 'CtrlRTS', 'CtrlCTS', 'DataData']:
-        #ev = if_events.mkevent(name, ev_dc={'src_addr':'100','dst_addr':'150'})
-        ev = if_events.mkevent(name, ev_dc={'src_addr':'100','dst_addr':'150'})
-        print ev
-        ev_q.put(ev, False)
-    print 'Input queue size', ev_q.qsize()
-
-    # create and start scheduler
-    print '\n=== Process ==='
-    sch.start()
-    ev_q.join()
-    sch.stop()
-    sch.join()   
- 
-
-    print '\n=== Read the output queue ==='
-    print 'Queue size:', frame_q.qsize()
-    while not frame_q.empty():
-        item = frame_q.get()
-        print "Frame ", repr(item)
-   
-    return
-
-
-
 if __name__ == '__main__':
+    import doctest
+    testfilename = sys.argv[0][:-2] + 'txt'
     try:
-        test()
-    except KeyboardInterrupt:
-        sys.exit()
+        doctest.testfile(testfilename)
+    except:      # no text file present
+        pass
 
--- gn/libadaptlay80211/schedFrToEv.py
+++ gn/libadaptlay80211/schedFrToEv.py
@@ -5,41 +5,38 @@
 #
 
 
-'''A frame to event scheduler, from Layer 1 into Layer 2.
+'''A frame to event scheduler, based on IEEE 802.11 frames.
 
 A scheduler that gets a frame from a Layer 1 input queue, generates the corresponding event, and puts it into Management, Control or Data queues based on the event type.
 '''
+
 import Queue
 import sys
 sys.path +=['..']
 
-import libutils.gnscheduler as Scheduler
-
-#
-#import libevents.evstrframes as evstrframes
+import libutils.gnscheduler as gnscheduler
 import libevents.if_events as if_events
 import libframes.if_frames as if_frames
 
 
 
-class SchedFrToEv(Scheduler.Scheduler):
+class SchedFrToEv(gnscheduler.Scheduler):
     '''Subclass of Scheduler for adapting layers 1 and 2.
     '''
 
     def fn_sched(self):
-        '''Scheduling function, reads frames, outputs evstrframes by type.
+        '''Scheduling function, reads frames, outputs events by type.
         
-        Reads one element from the input frame queue, generates an event, and puts the event in one of the output queues according to its type.
+        Scheduler function to process a frames queue: reads one element from the input frame queue, generates an event, and puts the event in one of the output queues according to its type.
+        
         out_queues: a dictionary of {nm_queue: (out_queue)}; nm_queue is a name for the queue, out_queue is the output queue.
         '''
         in_qu = self.in_queues[0]
         frame = in_qu.get(True)
 
-        #event = evstrframes.mkevent(pframe=frame)
         frm_obj = if_frames.objfrompkt(frame)
-        #
-        event = if_events.mkeventfromfrmobj(frm_obj)
-        #
+        event = if_events.frmtoev(frm_obj)
+
         if event != None:
             for item_type in self.out_queues.keys():
                 if  event.ev_type == item_type:
@@ -48,61 +45,23 @@ class SchedFrToEv(Scheduler.Scheduler):
                     out_queue.put(event, False)   # add to queue, don't block 
                     break
             else:
-                print 'Scheduler, event type not recognized:', event.ev_type
+                raise if_events.EventFrameException('Scheduler, event type \
+                    not recognized: ', event.ev_type)
         else:
-            print "Scheduler, erroneous frame"
+                raise if_events.EventFrameException('Scheduler, error in \
+                    frame')
         in_qu.task_done()
         return
 
 
 
-def test():
-    '''Tests SchedFrToEv class.
-
-    Put some frames in input queue to read, creates events and puts in different output queues according to the event type.
-    '''
-
-    # create input queue
-    frame_q = Queue.Queue(10)
-    ctrl_q, mgmt_q, data_q = Queue.Queue(10), Queue.Queue(10), Queue.Queue(10)
-    ls_out_queues = [ctrl_q, mgmt_q, data_q]
-    out_queues = { \
-        'Ctrl': (ctrl_q), \
-        'Mgmt': (mgmt_q), \
-        'Data': (data_q)  \
-        }
-    sch = SchedFrToEv(frame_q, out_queues)
-
-    # put events in input queue
-    for name in ['MgmtBeacon', 'CtrlRTS', 'CtrlCTS', 'DataData']:
-        ev = if_events.mkevent(name, ev_dc={'src_addr':'100','dst_addr':'150'})
-        frame = evstrframes.mkframe(ev)
-        print " Frame = ", frame
-        frame_q.put(frame,False)
-    print '=== Scheduler based on item type ==='
-    print 'Input queue size', frame_q.qsize()
-
-    # create and start scheduler
-    print '\n=== Process ==='
-    sch.start()
-    frame_q.join()
-    sch.stop()
-    sch.join()
-
-    print '\n=== Read the output queues ==='
-    for qu in ls_out_queues:
-        print 'Queue size:', qu.qsize()
-        while not qu.empty():
-            item = qu.get()
-            print "Event", item
-    return
-    
-    
-
-
 if __name__ == '__main__':
+    import doctest
+    testfilename = sys.argv[0][:-2] + 'txt'
     try:
-        test()
-    except KeyboardInterrupt:
-        sys.exit()
+        doctest.testfile(testfilename)
+    except:      # no text file present
+        pass
+
+
 
